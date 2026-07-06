fba1473c0d2fc5eac2722328fb86f18a732604b1
akrifisofficial-a11y <akrifisofficial@gmail.com>
2026-07-07 01:16:12 +0600

    Update config.js

--- config.js
+++ config.js
@@ -1,4 +1,4 @@
-const SUPABASE_URL = 'ВСТАВЬ_СВОЙ_URL';
-const SUPABASE_KEY = 'ВСТАВЬ_СВОЙ_ANON_KEY';
+const SUPABASE_URL = 'https://fkjqlcgsftkyerwnakkd.supabase.co';
+const SUPABASE_KEY = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImZranFsY2dzZnRreWVyd25ha2tkIiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODMyNjk4ODIsImV4cCI6MjA5ODg0NTg4Mn0.zrDG6Lsmrxr4eunZO1kXoZgRIwCAPcMmdYHy9NbuzGg';
 const supabase = window.supabase.createClient(SUPABASE_URL, SUPABASE_KEY);
 const ADMIN_PASSWORD = 'anivolume123'; // тот же что в БД
