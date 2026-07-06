81620b29d765848234b1a55d5d7afb5528009922
akrifisofficial-a11y <akrifisofficial@gmail.com>
2026-07-07 00:52:12 +0600

    Create config.js

---
+++ config.js
@@ -0,0 +1,4 @@
+const SUPABASE_URL = 'ВСТАВЬ_СВОЙ_URL';
+const SUPABASE_KEY = 'ВСТАВЬ_СВОЙ_ANON_KEY';
+const supabase = window.supabase.createClient(SUPABASE_URL, SUPABASE_KEY);
+const ADMIN_PASSWORD = 'anivolume123'; // тот же что в БД
